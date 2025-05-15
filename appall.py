d16757fc38cc972009d8a11fc2cd7fa55f3b8bb2
pouliassaf <pouliassaf@hotmail.com>
2025-05-15 23:30:08 +0400

    Update appall.py

--- appall.py
+++ appall.py
@@ -12,6 +12,8 @@ page = st.sidebar.selectbox("Choose a page:", ["Lesson Input", "Observation Anal
 if page == "Lesson Input":
     st.title("Weekly Lesson Observation Input Tool")
 
+    
+
 email = st.text_input("Enter your school email to continue")
 allowed_domains = ["@charterschools.ae", "@adek.gov.ae", "@nceducation.com"]
 
