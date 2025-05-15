2ca3e88107ab5091a87ff7ba3e0c51de8a84c9c7
pouliassaf <pouliassaf@hotmail.com>
2025-05-15 21:54:23 +0400

    Update appall.py

--- appall.py
+++ appall.py
@@ -151,9 +151,7 @@ elif page == "Observation Analytics":
                 
                 import io
                 reflection_buffer = io.StringIO()
-reflection_buffer.write("Reflection Summary Report
-
-")
+reflection_buffer.write("Reflection Summary Report\n")
 
 if not school_reflection.empty:
     reflection_buffer.write("School - Areas of Strength:
@@ -475,6 +473,7 @@ Observation Type: {obs_type}")
         with open(save_path, "rb") as f:
             st.download_button("📅 Download updated workbook", f, file_name=save_path)
         os.remove(save_path)
+
 
 
 
